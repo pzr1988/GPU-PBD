078801adef35292f29d7eda94af71333e233f7d5
pzr1988 <zherong.pan.usa@gmail.com>
2023-11-10 22:33:28 -0800

    initial commit with collision detector to be implemented

--- PBD/Collision.cu
+++ PBD/Collision.cu
@@ -6,6 +6,10 @@ CollisionDetector<T>::CollisionDetector(std::shared_ptr<Geometry<T>> geometry)
   :_geometry(geometry) {}
 template <typename T>
 void CollisionDetector<T>::detectCollisions() {
+  //To see how to implement this function, refer to the tutorial:
+  //http://media.steampowered.com/apps/valve/2015/DirkGregorius_Contacts.pdf
+  //To see a sample code of how this can be implemented on CPU, see:
+  //https://bitbucket.org/runningblade/libdifferentiable/src/ConvexHullPBAD/Environment/ContactGenerator.h
   FUNCTION_NOT_IMPLEMENTED
 }
 template <typename T>
