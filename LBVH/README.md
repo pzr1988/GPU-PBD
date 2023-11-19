bb0686ff4f1f6d292da75b736954b9a6aad5ab31
yetiliu <sjtuliuyuan@gmail.com>
2023-11-20 03:46:49 +0800

    init lbvh

---
+++ LBVH/README.md
@@ -0,0 +1,4 @@
+Modify from https://github.com/ToruNiina/lbvh
+
+https://developer.nvidia.com/blog/thinking-parallel-part-ii-tree-traversal-gpu/
+https://developer.nvidia.com/blog/thinking-parallel-part-iii-tree-construction-gpu/
--- Main/mainDebugCollision.cu
+++ Main/mainDebugCollision.cu
@@ -1,10 +1,12 @@
 #include <PBD/Utils.h>
 #include <iostream>
-
+#include <PBD/Geometry.h>
+#include <LBVH/morton_code.cuh>
 using namespace GPUPBD;
 
 int main() {
   typedef LSCALAR T;
   DECL_MAT_VEC_MAP_TYPES_T
+  std::cout<<"hello compile"<<std::endl;
   return 0;
 }
