bb5bd47585bf05017e7d203110d3a3b3ecde6641
yetiliu <sjtuliuyuan@gmail.com>
2023-12-05 11:08:25 -0800

    write file for tinyvisualizer

--- PBD/Geometry.h
+++ PBD/Geometry.h
@@ -23,6 +23,27 @@ struct Capsule {
   };
 };
 
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Capsule<T>& capsule) {
+  out << capsule._len << " " << capsule._radius << " ";
+  for (int i = 0; i < capsule._trans.rows(); ++i) {
+    for (int j = 0; j < capsule._trans.cols(); ++j) {
+      out << capsule._trans(i, j) << " ";
+    }
+  }
+  return out;
+}
+template <typename T>
+std::istream& operator>>(std::istream& in, Capsule<T>& capsule) {
+  in >> capsule._len >> capsule._radius;
+  for (int i = 0; i < capsule._trans.rows(); ++i) {
+    for (int j = 0; j < capsule._trans.cols(); ++j) {
+      in >> capsule._trans(i, j);
+    }
+  }
+  return in;
+}
+
 //This structure represents a collision between a pair of capsules
 //The index of first/second capsule is store in _capsuleIdA/_capsuleIdB
 //The points of contact in local coordinates are stored in _localPointA/_localPointB
@@ -41,6 +62,40 @@ struct Collision {
     return (_localPointA-_localPointB).dot(_globalNormal);
   }
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Collision<T>& collision) {
+  out << collision._capsuleIdA << " "
+      << collision._capsuleIdB << " ";
+  for (int i = 0; i < collision._localPointA.rows(); ++i) {
+    out << collision._localPointA(i) << " ";
+  }
+  for (int i = 0; i < collision._localPointB.rows(); ++i) {
+    out << collision._localPointB(i) << " ";
+  }
+  for (int i = 0; i < collision._globalNormal.rows(); ++i) {
+    out << collision._globalNormal(i) << " ";
+  }
+  out << collision._isValid;
+  return out;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& in, Collision<T>& collision) {
+  in >> collision._capsuleIdA
+      >> collision._capsuleIdB;
+  for (int i = 0; i < collision._localPointA.rows(); ++i) {
+    in >> collision._localPointA(i);
+  }
+  for (int i = 0; i < collision._localPointB.rows(); ++i) {
+    in >> collision._localPointB(i);
+  }
+  for (int i = 0; i < collision._globalNormal.rows(); ++i) {
+    in >> collision._globalNormal(i);
+  }
+  in >> collision._isValid;
+  return in;
+}
 
 //The geometry stores a vector of capsules
 //The vector is over-sized and pre-allocated
