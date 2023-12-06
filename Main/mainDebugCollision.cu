0a5c17a4c617bd7cd88f053fa966a50850e2ad2c
yetiliu <sjtuliuyuan@gmail.com>
2023-12-06 15:27:15 -0800

    加入写文件，供调试使用

--- Main/mainDebugCollision.cu
+++ Main/mainDebugCollision.cu
@@ -2,6 +2,9 @@
 #include <PBD/Geometry.h>
 #include <PBD/Collision.h>
 #include <random>
+#include <fstream>
+#include <iostream>
+
 
 using namespace GPUPBD;
 
@@ -15,12 +18,13 @@ int main() {
 
   for(auto& p : ps) {
     p._len = uni(mt);
-    p._radius = uni(mt);
+    p._radius = uni(mt)/3.0;
     Eigen::Quaternion<T> q;
     q.x() = uni(mt);
     q.y() = uni(mt);
     q.z() = uni(mt);
     q.w() = uni(mt);
+    q.normalize();
     Eigen::Matrix<T, 3, 3> rot = q.toRotationMatrix();
     Eigen::Matrix<T, 3, 1> trans;
     trans.setRandom();
@@ -33,5 +37,28 @@ int main() {
   geometry->setCapsule(ps);
   GPUPBD::CollisionDetector<T> detector(geometry);
   detector.detectCollisions();
+
+  // write file for TinyVisualizer
+  std::ofstream file("collision.txt");
+  // 格式：Capsule _radius _len _trans
+  if (!file.is_open()) {
+    std::cerr << "Unable to open file" << std::endl;
+    return; // 或者使用其他错误处理方法
+  }
+  for(auto& p : ps) {
+    file << "Capsule" << std::endl;
+    file << p << std::endl;
+  }
+
+  size_t num_collision = detector.size();
+  for(size_t i = 0; i < num_collision; i++) {
+    auto c = detector[i];
+    file << "Collision" << std::endl;
+    file << c << std::endl;
+  }
+
+  file.close();
+
+
   return 0;
 }
