c390bdfa0b2729edc5e71de866371be00ffe3745
yetiliu <sjtuliuyuan@gmail.com>
2024-01-02 11:01:03 -0800

    fix

--- PBD/XPBD.cu
+++ PBD/XPBD.cu
@@ -61,16 +61,18 @@ void XPBD<T>::relaxConstraint() {
     auto& collision = d_collisions[idx];
     auto& cA = d_capsules[collision._capsuleIdA];
     auto& cB = d_capsules[collision._capsuleIdB];
-    auto globalPointA = cA._q.toRotationMatrix()*collision._localPointA+cA._x;
-    auto globalPointB = cB._q.toRotationMatrix()*collision._localPointB+cB._x;
-    auto cARCrossN = globalPointA.cross(collision._globalNormal); //r_1 x n
+    auto placementPointA = cA._q.toRotationMatrix()*collision._localPointA;
+    auto placementPointB = cB._q.toRotationMatrix()*collision._localPointB;
+    auto cARCrossN = placementPointA.cross(collision._globalNormal); //r_1 x n
     auto cAR = cA._q.toRotationMatrix();
     auto cAIinv = cAR*cA._Ibodyinv*cAR.transpose(); //inverse of interia tensor
-    auto cBRCrossN = globalPointB.cross(collision._globalNormal); //r_2 x n
+    auto cBRCrossN = placementPointB.cross(collision._globalNormal); //r_2 x n
     auto cBR = cB._q.toRotationMatrix();
     auto cBIinv = cBR*cB._Ibodyinv*cBR.transpose(); //inverse of interia tensor
     T wA = 1.0/cA._mass+cARCrossN.transpose()*cAIinv*cARCrossN;
     T wB = 1.0/cB._mass+cBRCrossN.transpose()*cBIinv*cBRCrossN;
+    auto globalPointA = placementPointA+cA._x;
+    auto globalPointB = placementPointB+cB._x;
     T c = (globalPointA-globalPointB).dot(collision._globalNormal);
     T alpha= collision._alpha / (dt*dt);
     T deltaLambda = (-c-d_lambda[idx]*alpha)/(wA + wB + alpha);
@@ -79,12 +81,12 @@ void XPBD<T>::relaxConstraint() {
     // TODO 原子
     cA._x += p/cA._mass;
     cB._x -= p/cB._mass;
-    Vec3T cAIinvRCrossP = cAIinv * (globalPointA.cross(p)); // I^{-1}(r x p)
+    Vec3T cAIinvRCrossP = cAIinv * (placementPointA.cross(p)); // I^{-1}(r x p)
     Eigen::Quaternion<T> cAIinvRCrossPQuat(0,cAIinvRCrossP.x(),cAIinvRCrossP.y(),cAIinvRCrossP.z());
     Eigen::Quaternion<T> cAQUpdated = Eigen::Quaternion<T>(0.5,0,0,0)*cAIinvRCrossPQuat*cA._q;
     cA._q = Eigen::Quaternion<T>(cA._q.coeffs() + cAQUpdated.coeffs());
     cA._q.normalize();
-    Vec3T cBIinvRCrossP = cBIinv * (globalPointB.cross(p)); // I^{-1}(r x p)
+    Vec3T cBIinvRCrossP = cBIinv * (placementPointB.cross(p)); // I^{-1}(r x p)
     Eigen::Quaternion<T> cBIinvRCrossPQuat(0,cBIinvRCrossP.x(),cBIinvRCrossP.y(),cBIinvRCrossP.z());
     Eigen::Quaternion<T> cBQUpdated = Eigen::Quaternion<T>(0.5,0,0,0)*cBIinvRCrossPQuat*cB._q;
     cB._q = Eigen::Quaternion<T>(cB._q.coeffs() - cBQUpdated.coeffs());
