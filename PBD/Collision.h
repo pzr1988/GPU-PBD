a3bd43fb60e474a10b20f58903c03f47f53b0fcd
zherong <zherong@cs.unc.edu>
2024-01-05 23:21:15 -0800

    some minor fix-up

--- PBD/Collision.h
+++ PBD/Collision.h
@@ -4,6 +4,26 @@
 #include <LBVH/lbvh.cuh>
 
 namespace GPUPBD {
+
+//This structure represents a collision between a pair of capsules
+//The index of first/second capsule is store in _capsuleIdA/_capsuleIdB
+//The points of contact in local coordinates are stored in _localPointA/_localPointB
+//The separating direction of contact is stored in _globalNormal, extending from A to B and having unit norm
+template <typename T>
+struct Collision {
+  DECL_MAT_VEC_MAP_TYPES_T
+  int _capsuleIdA;
+  int _capsuleIdB;
+  Vec3T _localPointA;
+  Vec3T _localPointB;
+  Vec3T _globalNormal;
+  T _alpha; // compliance of the constraint
+  bool _isValid;
+  DEVICE_HOST Collision():_capsuleIdA(-1),_capsuleIdB(-1),
+    _localPointA(Vec3T()),_localPointB(Vec3T()),_globalNormal(Vec3T()),
+    _alpha(.0001), _isValid(false) {}
+};
+
 //The collisionDetector has the capability of detecting all pairs of collisions between all pairs of capsules
 //The main function (detectCollisions()) is supposed to fill up the vector _collisions with active collision constraints
 template <typename T>
--- PBD/Geometry.h
+++ PBD/Geometry.h
@@ -65,25 +65,6 @@ struct Capsule {
   }
 };
 
-//This structure represents a collision between a pair of capsules
-//The index of first/second capsule is store in _capsuleIdA/_capsuleIdB
-//The points of contact in local coordinates are stored in _localPointA/_localPointB
-//The separating direction of contact is stored in _globalNormal, extending from A to B and having unit norm
-template <typename T>
-struct Collision {
-  DECL_MAT_VEC_MAP_TYPES_T
-  int _capsuleIdA;
-  int _capsuleIdB;
-  Vec3T _localPointA;
-  Vec3T _localPointB;
-  Vec3T _globalNormal;
-  T _alpha; // compliance of the constraint
-  bool _isValid;
-  DEVICE_HOST Collision():_capsuleIdA(-1),_capsuleIdB(-1),
-    _localPointA(Vec3T()),_localPointB(Vec3T()),_globalNormal(Vec3T()),
-    _alpha(.0001), _isValid(false) {}
-};
-
 //The geometry stores a vector of capsules
 //The vector is over-sized and pre-allocated
 //The number of capsules in use is stored in _nrCapsule
