0422cc6998955273718fbe8a52a0d0d80b4d1f8f
yetiliu <sjtuliuyuan@gmail.com>
2024-01-11 11:46:23 -0800

    capsule as node, collision as edge. split graph by connectivity. parallel cross sub-graph, serial in sub-graph.

--- PBD/XPBD.cu
+++ PBD/XPBD.cu
@@ -36,21 +36,77 @@ void XPBD<T>::integrate() {
   });
   cudaDeviceSynchronize();
 }
+__device__ int binary_search(const int* data, int left, int right, int value) {
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (data[mid] == value) {
+      return mid;
+    } else if (data[mid] < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+template <typename T>
+struct UpdateLabels {
+  int n;
+  int* idMap;
+  int* labels;
+  UpdateLabels(int _n, int* _idMap, int* _labels) : n(_n), idMap(_idMap), labels(_labels) {}
+  __device__ bool operator()(const Collision<T>& c) {
+    int u = binary_search(idMap, 0, n-1, c._capsuleIdA);
+    int v = binary_search(idMap, 0, n-1, c._capsuleIdB);
+    int label_u = labels[u];
+    int label_v = labels[v];
+
+    if (label_u != label_v) {
+      int min_label = min(label_u, label_v);
+      atomicMin(&labels[u], min_label);
+      atomicMin(&labels[v], min_label);
+      return true;
+    }
+    return false;
+  }
+};
 template <typename T>
 void XPBD<T>::initRelaxConstraint() {
   if(_detector->size() == 0) {
     return;
   }
   _lambda.clear();
-  _lambda.resize(_detector->size());
-  _collisionCapsuleId.resize(_detector->size()*2); //each collision contains 2 capsules
-  _number.resize(_detector->size()*2);
-  _deltaX.resize(_detector->size()*2);
-  _deltaQ.resize(_detector->size()*2);
-  _reduceCapsuleId.resize(_detector->size()*2);
-  _reduceNumber.resize(_detector->size()*2);
-  _reduceDeltaX.resize(_detector->size()*2);
-  _reduceDeltaQ.resize(_detector->size()*2);
+  int collisionNum = _detector->size();
+  _lambda.resize(collisionNum);
+  _changes.resize(collisionNum);
+  _capsuleIds.resize(collisionNum*2);
+  int* d_capsuleIds = thrust::raw_pointer_cast(_capsuleIds.data());
+  const auto& collisions = _detector->getCollisions();
+  const Collision<T>* d_collisions = thrust::raw_pointer_cast(collisions.data());
+  thrust::for_each(thrust::device,
+                   thrust::make_counting_iterator(0),
+                   thrust::make_counting_iterator(collisionNum),
+  [=] __host__ __device__ (int idx) {
+    auto& collision = d_collisions[idx];
+    d_capsuleIds[2*idx] = collision._capsuleIdA;
+    d_capsuleIds[2*idx+1] = collision._capsuleIdB;
+  });
+  auto endUnique = thrust::unique(_capsuleIds.begin(), _capsuleIds.end());
+  _capsuleIds.resize(thrust::distance(_capsuleIds.begin(), endUnique));
+  int numCapsuleIds = _capsuleIds.size();
+  thrust::sort(_capsuleIds.begin(), _capsuleIds.end());
+  _labels.resize(numCapsuleIds);
+  thrust::copy(_capsuleIds.begin(), _capsuleIds.end(), _labels.begin());
+  int* d_labels = thrust::raw_pointer_cast(_labels.data());
+  bool changed;
+  do {
+    thrust::transform(thrust::device, collisions.begin(), collisions.end(),
+                      _changes.begin(), UpdateLabels<T>(numCapsuleIds, d_capsuleIds, d_labels));
+    changed = thrust::reduce(thrust::device, _changes.begin(), _changes.end(), false, thrust::logical_or<bool>());
+  } while (changed);
+  _uniqueLabels.resize(_labels.size());
+  auto endUniqueLabels = thrust::unique_copy(_labels.begin(), _labels.end(), _uniqueLabels.begin());
+  _uniqueLabels.resize(thrust::distance(_uniqueLabels.begin(), endUniqueLabels));
 
 }
 template <typename T>
@@ -59,46 +115,64 @@ void XPBD<T>::relaxConstraint() {
     return;
   }
   const auto& collisions = _detector->getCollisions();
+  int numCollision = collisions.size();
   const Collision<T>* d_collisions = thrust::raw_pointer_cast(collisions.data());
   auto& capsules = _geometry->getMutableCapsules();
   Capsule<T>* d_capsules = thrust::raw_pointer_cast(capsules.data());
   T* d_lambda = thrust::raw_pointer_cast(_lambda.data());
-  int* d_collisionCapsuleId = thrust::raw_pointer_cast(_collisionCapsuleId.data());
-  Vec3T* d_deltaX = thrust::raw_pointer_cast(_deltaX.data());
-  Vec4T* d_deltaQ = thrust::raw_pointer_cast(_deltaQ.data());
   T dt = _dt;
+  int numCapsuleIds = _capsuleIds.size();
+  int* d_capsuleIds = thrust::raw_pointer_cast(_capsuleIds.data());
+  int* d_labels = thrust::raw_pointer_cast(_labels.data());
+  _labelOfCollision.resize(numCollision);
+  int* d_labelOfCollision = thrust::raw_pointer_cast(_labelOfCollision.data());
   thrust::for_each(thrust::device,
-                   thrust::make_counting_iterator(0),
-                   thrust::make_counting_iterator(static_cast<int>(collisions.size())),
-  [=] __host__ __device__ (int idx) {
+                   thrust::make_counting_iterator<std::size_t>(0),
+                   thrust::make_counting_iterator<std::size_t>(numCollision),
+  [=] __device__ (int idx) {
     auto& collision = d_collisions[idx];
-    auto& cA = d_capsules[collision._capsuleIdA];
-    auto& cB = d_capsules[collision._capsuleIdB];
-    auto placementPointA = cA._q.toRotationMatrix()*collision._localPointA;
-    auto placementPointB = cB._q.toRotationMatrix()*collision._localPointB;
-    auto globalPointA = placementPointA+cA._x;
-    auto globalPointB = placementPointB+cB._x;
-    auto wA = computeGeneralizedInversMass(cA, placementPointA,
-                                           collision._globalNormal);
-    auto wB = computeGeneralizedInversMass(cB, placementPointB,
-                                           collision._globalNormal);
-    auto collisionDepth = (globalPointA-globalPointB).dot(collision._globalNormal);
-    if(collisionDepth<=0)
-      return;
-    auto alpha = collision._alpha/(dt*dt);
-    auto deltaLambda = (-collisionDepth-d_lambda[idx]*alpha)/(wA+wB+alpha);
-    d_lambda[idx] += deltaLambda;
-    auto pulse = deltaLambda*collision._globalNormal;
-    // To avoid multi write problem, first cache update
-    d_collisionCapsuleId[2*idx] = collision._capsuleIdA;
-    d_collisionCapsuleId[2*idx+1] = collision._capsuleIdB;
-    d_deltaX[2*idx] = pulse/cA._mass;
-    d_deltaX[2*idx+1] = -pulse/cB._mass;
-    d_deltaQ[2*idx] = getDeltaRot(cA, placementPointA, pulse).coeffs();
-    d_deltaQ[2*idx+1] = -getDeltaRot(cB, placementPointB, pulse).coeffs();
+    d_labelOfCollision[idx] = d_labels[binary_search(d_capsuleIds, 0, numCapsuleIds, collision._capsuleIdA)];
   });
-
-  updateCapsuleState();
+  thrust::for_each(thrust::device,
+                   _uniqueLabels.begin(),
+                   _uniqueLabels.end(),
+  [=] __device__ (int targetLabel) {
+    for(int idx=0; idx < numCollision; idx++) {
+      int label = d_labels[binary_search(d_capsuleIds, 0, numCapsuleIds, d_collisions[idx]._capsuleIdA)];
+      if(label == targetLabel) {
+        auto& collision = d_collisions[idx];
+        auto& cA = d_capsules[collision._capsuleIdA];
+        auto& cB = d_capsules[collision._capsuleIdB];
+        auto placementPointA = cA._q.toRotationMatrix()*collision._localPointA;
+        auto placementPointB = cB._q.toRotationMatrix()*collision._localPointB;
+        auto globalPointA = placementPointA+cA._x;
+        auto globalPointB = placementPointB+cB._x;
+        auto wA = computeGeneralizedInversMass(cA, placementPointA,
+                                               collision._globalNormal);
+        auto wB = computeGeneralizedInversMass(cB, placementPointB,
+                                               collision._globalNormal);
+        auto collisionDepth = (globalPointA-globalPointB).dot(collision._globalNormal);
+        if(collisionDepth<=0)
+          return;
+        auto alpha = collision._alpha/(dt*dt);
+        auto deltaLambda = (-collisionDepth-d_lambda[idx]*alpha)/(wA+wB+alpha);
+        d_lambda[idx] += deltaLambda;
+        auto pulse = deltaLambda*collision._globalNormal;
+        if(cA._isDynamic) {
+          cA._x += pulse/cA._mass;
+          auto cAQUpdated = getDeltaRot(cA, placementPointA, pulse);
+          cA._q = Eigen::Quaternion<T>(cA._q.coeffs()+cAQUpdated.coeffs());
+          cA._q.normalize();
+        }
+        if(cB._isDynamic) {
+          cB._x -= pulse/cB._mass;
+          auto cBQUpdated = getDeltaRot(cB, placementPointB, pulse);
+          cB._q = Eigen::Quaternion<T>(cB._q.coeffs()-cBQUpdated.coeffs());
+          cB._q.normalize();
+        }
+      }
+    }
+  });
 }
 template <typename T>
 void XPBD<T>::updateVelocity() {
@@ -139,47 +213,6 @@ DEVICE_HOST Eigen::Quaternion<T> XPBD<T>::getDeltaRot(const Capsule<T>& c, const
   Eigen::Quaternion<T> cIinvRCrossPQuat(0,cIinvRCrossP.x(),cIinvRCrossP.y(),cIinvRCrossP.z());
   auto qUpdated = Eigen::Quaternion<T>(0.5,0,0,0)*cIinvRCrossPQuat*c._q;
   return qUpdated;
-}
-template <typename T>
-void XPBD<T>::updateCapsuleState() {
-  auto& capsules = _geometry->getMutableCapsules();
-  Capsule<T>* d_capsules = thrust::raw_pointer_cast(capsules.data());
-  //Reduce multi collisions of one capsule, then write
-  thrust::fill(_number.begin(), _number.end(), 1);
-  thrust::reduce_by_key(_collisionCapsuleId.begin(), _collisionCapsuleId.end(),
-                        _number.begin(), _number.begin(), _reduceNumber.begin());
-  int * d_reduceNumber = thrust::raw_pointer_cast(_reduceNumber.data());
-
-  auto endX = thrust::reduce_by_key(_collisionCapsuleId.begin(), _collisionCapsuleId.end(),
-                                    _deltaX.begin(), _reduceCapsuleId.begin(), _reduceDeltaX.begin());
-  _reduceCapsuleId.erase(endX.first, _reduceCapsuleId.end());
-  int * d_reduceCapsuleId = thrust::raw_pointer_cast(_reduceCapsuleId.data());
-  Vec3T* d_reduceDeltaX = thrust::raw_pointer_cast(_reduceDeltaX.data());
-  thrust::for_each(thrust::device,
-                   thrust::make_counting_iterator(0),
-                   thrust::make_counting_iterator(static_cast<int>(_reduceCapsuleId.size())),
-  [=] __host__ __device__ (int idx) {
-    if(d_capsules[d_reduceCapsuleId[idx]]._isDynamic) {
-      d_capsules[d_reduceCapsuleId[idx]]._x = d_capsules[d_reduceCapsuleId[idx]]._x
-                                              + d_reduceDeltaX[idx]/static_cast<float>(d_reduceNumber[idx]);
-    }
-  });
-
-  auto endQ = thrust::reduce_by_key(_collisionCapsuleId.begin(), _collisionCapsuleId.end(),
-                                    _deltaQ.begin(), _reduceCapsuleId.begin(), _reduceDeltaQ.begin());
-  _reduceCapsuleId.erase(endQ.first, _reduceCapsuleId.end());
-  d_reduceCapsuleId = thrust::raw_pointer_cast(_reduceCapsuleId.data());
-  Vec4T* d_reduceDeltaQ = thrust::raw_pointer_cast(_reduceDeltaQ.data());
-  thrust::for_each(thrust::device,
-                   thrust::make_counting_iterator(0),
-                   thrust::make_counting_iterator(static_cast<int>(_reduceCapsuleId.size())),
-  [=] __host__ __device__ (int idx) {
-    if(d_capsules[d_reduceCapsuleId[idx]]._isDynamic) {
-      d_capsules[d_reduceCapsuleId[idx]]._q = Eigen::Quaternion<T>(d_capsules[d_reduceCapsuleId[idx]]._q.coeffs()
-                                              + d_reduceDeltaQ[idx]/static_cast<float>(d_reduceNumber[idx]));
-      d_capsules[d_reduceCapsuleId[idx]]._q.normalize();
-    }
-  });
 }
 //declare instance
 template struct XPBD<LSCALAR>;
--- PBD/XPBD.h
+++ PBD/XPBD.h
@@ -16,37 +16,30 @@ class XPBD {
   void step();
   //Initial time integration for updating Capsule._transNext
   void integrate();
+  //Init for relax all the constraints process
+  void initRelaxConstraint();
   //Relax all the constraints
   void relaxConstraint();
   //During relaxConstraints, the state's update will be cached.
   //Then we update state in this function.
-  void updateCapsuleState();
-  //We first compute Capsule._v and Capsule._w from Capsule._trans(Next)
-  //We then set Capsule._trans=Capsule._transNext;
   void updateVelocity();
   //Use detector to visulize collisions
   const CollisionDetector<T>& getDetector() const;
+  DEVICE_HOST static T computeGeneralizedInversMass(const Capsule<T>& capsule, const Vec3T& normal, const Vec3T& placementPoint);
+  DEVICE_HOST static Eigen::Quaternion<T> getDeltaRot(const Capsule<T>& capsule, const Vec3T& placementPoint, const Vec3T& pulse);
  protected:
   std::shared_ptr<Geometry<T>> _geometry;
   std::shared_ptr<CollisionDetector<T>> _detector;
   T _dt;
   int _nRelax;
  private:
-  //Init for relax all the constraints process
-  void initRelaxConstraint();
-  DEVICE_HOST static T computeGeneralizedInversMass(const Capsule<T>& capsule, const Vec3T& normal, const Vec3T& placementPoint);
-  DEVICE_HOST static Eigen::Quaternion<T> getDeltaRot(const Capsule<T>& capsule, const Vec3T& placementPoint, const Vec3T& pulse);
  private:
   thrust::device_vector<T> _lambda;
-  //Cache deltaX and deltaQ during relaxConstraint to avoid multi write problem
-  thrust::device_vector<int> _collisionCapsuleId;
-  thrust::device_vector<int> _number;
-  thrust::device_vector<Vec3T> _deltaX;
-  thrust::device_vector<Vec4T> _deltaQ;
-  thrust::device_vector<int> _reduceCapsuleId;
-  thrust::device_vector<int> _reduceNumber;
-  thrust::device_vector<Vec3T> _reduceDeltaX;
-  thrust::device_vector<Vec4T> _reduceDeltaQ;
+  thrust::device_vector<int> _capsuleIds;
+  thrust::device_vector<int> _labels;
+  thrust::device_vector<int> _uniqueLabels;
+  thrust::device_vector<bool> _changes;
+  thrust::device_vector<int> _labelOfCollision;
 };
 
 }
