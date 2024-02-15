e38be838540f9f5e420ee10e12cc632a99edb370
yetiliu <sjtuliuyuan@gmail.com>
2024-02-15 14:24:46 -0800

    read capsule position finished

--- Main/mainSK.cu
+++ Main/mainSK.cu
@@ -17,8 +17,10 @@ struct Joint {
   DECL_MAT_VEC_MAP_TYPES_T
   int _parent;
   int _depth;
+  T _radius;
   Vec3T _x;
   QuatT _q;
+  Vec6T _ft;
   std::string _name;
   Capsule<T> _c;
 };
@@ -33,18 +35,15 @@ void readJoints(std::vector<Joint<T>>& joints, int parentId, const tinyxml2::XML
   //read trans
   {
     joint._x=PHYSICSMOTION::parsePtreeDef<Vec3T>(*g,"<xmlattr>.pos","0 0 0");
-    joint._q=PHYSICSMOTION::parsePtreeDef<Vec4T>(*g,"<xmlattr>.quat","1 0 0 0");
+    Vec4T tmpQ=PHYSICSMOTION::parsePtreeDef<Vec4T>(*g,"<xmlattr>.quat","1 0 0 0");
+    joint._q=QuatT(tmpQ[0],tmpQ[1],tmpQ[2],tmpQ[3]);
   }
   //read geometry
   if(g->FirstChildElement("geom")->FindAttribute("type") == NULL) {
     //capsule
     const tinyxml2::XMLElement* gg=g->FirstChildElement("geom");
-    Vec6T ft=PHYSICSMOTION::parsePtreeDef<Vec6T>(*gg,"<xmlattr>.fromto","0 0 0 0 0 0");
-    joint._c._radius=PHYSICSMOTION::get<T>(*gg,"<xmlattr>.size");
-    joint._c._len=Vec3T(ft[3]-ft[0], ft[4]-ft[1],ft[5]-ft[2]).norm();
-    joint._c._x=Vec3T(ft[3]+ft[0], ft[4]+ft[1],ft[5]+ft[2])/2;
-    joint._c._q=QuatT::FromTwoVectors(Vec3T::UnitX(),Vec3T(ft[3]-ft[0], ft[4]-ft[1],ft[5]-ft[2]).normalized());
-
+    joint._ft=PHYSICSMOTION::parsePtreeDef<Vec6T>(*gg,"<xmlattr>.fromto","0 0 0 0 0 0");
+    joint._radius=PHYSICSMOTION::get<T>(*gg,"<xmlattr>.size");
     int _parentId=(int)joints.size();
     joints.push_back(joint);
     for(const tinyxml2::XMLElement* gc=g->FirstChildElement(); gc; gc=gc->NextSiblingElement())
@@ -65,16 +64,23 @@ void readMJCF(std::vector<Joint<T>>& joints, const std::string& file) {
 
 template <typename T>
 void updateCapsule(std::vector<Joint<T>>& joints) {
-  for(auto& j : joints) {
-    j._c._x = j._x + j._q.toRotationMatrix()*j._c._x;
-    j._c._q = (j._c._q * j._q).normalized();
-    if(j._parent>=0) {
-      const auto& p = joints[j._parent]._c;
-      j._x = p._x + p._q.toRotationMatrix()*j._x;
-      j._q = (j._q * p._q).normalized();
-      j._c._x = p._x + p._q.toRotationMatrix()*j._c._x;
-      j._c._q = (j._c._q * p._q).normalized();
+  DECL_MAT_VEC_MAP_TYPES_T
+  for(auto& joint : joints) {
+    Vec3T c1 = Vec3T(joint._ft[0], joint._ft[1], joint._ft[2]);
+    Vec3T c2 = Vec3T(joint._ft[3], joint._ft[4], joint._ft[5]);
+    c1 = joint._x + joint._q.toRotationMatrix() * c1;
+    c2 = joint._x + joint._q.toRotationMatrix() * c2;
+    if(joint._parent>=0) {
+      const auto& p = joints[joint._parent];
+      c1 = p._x + p._q.toRotationMatrix()*c1;
+      c2 = p._x + p._q.toRotationMatrix()*c2;
+      joint._x = p._x + p._q.toRotationMatrix()*joint._x;
+      joint._q = (p._q * joint._q).normalized();
     }
+    joint._c._radius=joint._radius;
+    joint._c._len=(c2-c1).norm();
+    joint._c._x=(c1+c2)/2;
+    joint._c._q=QuatT::FromTwoVectors(Vec3T::UnitX(),(c2-c1).normalized());
   }
 }
 
@@ -88,12 +94,10 @@ int main(int argc,char** argv) {
   for(int i=0; i<joints.size(); i++) {
     Joint<T>& j = joints[i];
     std::string indent(j._depth * 2, ' ');
-    auto tmpx = j._c._q.toRotationMatrix() * Vec3T(j._c._len, 0, 0);
     std::cout << indent  << j._name <<": x:" << j._x[0] << " " << j._x[1] << " " << j._x[2]
               << ", q:" << j._q.w() << " " << j._q.x() << " " << j._q.y() << " " << j._q.z()
-              << ", capsule radius: " << j._c._radius << ", len:" << j._c._len
-              <<", x:" << j._c._x[0] << " " << j._c._x[1] << " " << j._c._x[2]
-              << ", q:" << j._c._q.w() << " " << j._c._q.x() << " " << j._c._q.y() << " " << j._c._q.z()
+              << ", radius:" << j._radius
+              << ", fromto:" << j._ft[0] << " " << j._ft[1] << " "<< j._ft[2] << " "<< j._ft[3] << " "<< j._ft[4] << " "<< j._ft[5]
               << ", parent Name: " << joints[j._parent>-1?j._parent:0]._name
               << std::endl;
   }
